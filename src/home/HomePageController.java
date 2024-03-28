4a6ec890e6a197fa362b10bf3bb12bfa798bb877
Riley Short <rileyshort1@gmail.com>
2024-03-28 00:22:54 -0700

    Added "New Storage location page and connected it to DB"

--- src/home/HomePageController.java
+++ src/home/HomePageController.java
@@ -20,4 +20,11 @@ public class HomePageController {
         stage.setScene(scene);
         stage.show();
     }
+    public void switchToNewStorageLocationPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../newstoragelocation/NewStorageLocationPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/newstoragelocation/NewStorageLocationController.java
@@ -0,0 +1,51 @@
+package newstoragelocation;
+
+import database.DBConnect;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NewStorageLocationController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private TextField newLocationInput;
+    @FXML
+    private TextField locationDescriptionInput;
+    @FXML
+    private Button submit;
+
+    public void submitCategory(ActionEvent event) {
+
+        String location = newLocationInput.getText();
+        String description = locationDescriptionInput.getText();
+
+        DBConnect db = DBConnect.getDBInstance();
+        db.addStorageLocation(location, description);
+
+        try {
+            switchToHomePage(event);
+        }
+        catch (IOException e){
+            System.out.print(e.toString());
+        }
+    }
+
+    public void switchToHomePage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../home/HomePage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
