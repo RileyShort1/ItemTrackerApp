d6b790430f5f5bd9fac8e67335ad9e15f7bbca1c
Riley Short <rileyshort1@gmail.com>
2024-03-27 22:30:39 -0700

    Added csv files to gitignore

--- src/database/DBConnect.java
+++ src/database/DBConnect.java
@@ -1,13 +1,22 @@
 package database;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class DBConnect {
     private static DBConnect singleInstance = new DBConnect();
 
-    private DBConnect() {}
+    private DBConnect() {};
 
     public static DBConnect getDBInstance() {
         return singleInstance;
     }
+
+    public void addCategory(String category) {
+
+    }
+
+
 
 
 }
